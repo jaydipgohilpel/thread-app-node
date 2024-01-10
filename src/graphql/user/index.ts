bfc01f95cd61826542f1304d131f23ea6492fca3
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2024-01-10 16:12:42 +0530

    Revert "refactor the code"
    This reverts commit 9c18af54106dc19d1bce722ff72ab569bb152151.

--- src/graphql/user/index.ts
+++
@@ -1,6 +0,0 @@
-import { mutations } from './mutations'
-import { queries } from './queries'
-import { resolvers } from './resolvers'
-import { typeDefs } from './typedef'
-
-export const User = { mutations, queries, resolvers, typeDefs }
--- src/graphql/user/resolvers.ts
+++
@@ -1,6 +0,0 @@
-const queries = {}
-const mutations = {
-    createUser: async (_: any, { }, { }) => { return 'createUser' },
-}
-
-export const resolvers = { queries, mutations };
